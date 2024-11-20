c18088a296f534437f70bcff95394086ad02ed34
Rasan Rasch <rasan@nyu.edu>
2024-11-20 13:41:20 -0500

    Only use cookies for authentication

--- twscrape.go
+++ twscrape.go
@@ -5,19 +5,53 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
-	twitterscraper "github.com/n0madic/twitter-scraper"
+// 	twitterscraper "github.com/n0madic/twitter-scraper"
+	twitterscraper "github.com/imperatrona/twitter-scraper"
 	toml "github.com/pelletier/go-toml"
-	"github.com/sirupsen/logrus"
+	log "github.com/sirupsen/logrus"
+	"github.com/ssgelm/cookiejarparser"
 	"github.com/syndtr/goleveldb/leveldb"
 	prefixed "github.com/x-cray/logrus-prefixed-formatter"
 	"html"
 	"net/http"
 	"net/smtp"
+	"net/url"
 	"os"
 	"path"
 	"strings"
 	"time"
 )
+
+
+func ReadJSONCookies(cookiesPath string) []*http.Cookie {
+	f, _ := os.Open(cookiesPath)
+	// deserialize from JSON
+	var cookies []*http.Cookie
+	json.NewDecoder(f).Decode(&cookies)
+	if err := f.Close(); err != nil {
+		panic(err)
+	}
+	return cookies
+}
+
+
+func ReadNetscapeCookies(cookiesPath string) []*http.Cookie {
+	cookiejar, err := cookiejarparser.LoadCookieJarFile(cookiesPath)
+	if err != nil {
+		panic(err)
+	}
+	u, _ := url.Parse("https://x.com")
+	cookies := cookiejar.Cookies(u)
+	return cookies
+}
+
+
+func PrintCookies(cookies []*http.Cookie) {
+	for _, cookie := range cookies {
+		log.Debug("Cookie ", cookie.Name, ": ", cookie.Value)
+	}
+}
+
 
 func SendTweet(
 	server string,
@@ -41,45 +75,59 @@ func SendTweet(
 	}
 }
 
+
 func usage() {
 	fmt.Fprintf(os.Stderr, "Usage: %s <twitter handle>\n\n", os.Args[0])
 }
+
+
+func init() {
+	log.SetFormatter(&prefixed.TextFormatter{
+		DisableColors:   true,
+		TimestampFormat: "2006-01-02 15:04:05",
+		FullTimestamp:   true,
+		ForceFormatting: true,
+	})
+	log.SetOutput(os.Stderr)
+}
+
 
 func main() {
 	debug := flag.Bool("debug", false, "enable debugging")
 	flag.Parse()
 
-	var level logrus.Level
+	var level log.Level
 	if *debug {
-		level = logrus.DebugLevel
+		level = log.DebugLevel
 	} else {
-		level = logrus.InfoLevel
+		level = log.InfoLevel
 	}
+	log.SetLevel(level)
 
 	if flag.NArg() != 1 {
 		usage()
 		os.Exit(1)
 	}
 
-	logger := &logrus.Logger{
-		Out:   os.Stderr,
-		Level: level,
-		Formatter: &prefixed.TextFormatter{
-			DisableColors:   true,
-			TimestampFormat: "2006-01-02 15:04:05",
-			FullTimestamp:   true,
-			ForceFormatting: true,
-		},
-	}
+// 	logger := &logrus.Logger{
+// 		Out:   os.Stderr,
+// 		Level: level,
+// 		Formatter: &prefixed.TextFormatter{
+// 			DisableColors:   true,
+// 			TimestampFormat: "2006-01-02 15:04:05",
+// 			FullTimestamp:   true,
+// 			ForceFormatting: true,
+// 		},
+// 	}
 
 	twitterHandle := flag.Arg(0)
 
-	dirname, err := os.UserHomeDir()
+	homedir, err := os.UserHomeDir()
 	if err != nil {
 		panic(err)
 	}
 
-	tomlPath := path.Join(dirname, "work", "python-scripts", "config.toml")
+	tomlPath := path.Join(homedir, "work", "python-scripts", "config.toml")
 	config, err := toml.LoadFile(tomlPath)
 	if err != nil {
 		panic(err)
@@ -97,15 +145,15 @@ func main() {
 	mailFrom := config.Get("main.mailfrom").(string)
 	mailTo := config.GetArray("main.mailto").([]string)
 
-	logger.Debug("mail server: ", mailServer)
-	logger.Debug("mail from: ", mailFrom)
-	logger.Debug("mail to: ", mailTo)
+	log.Debug("mail server: ", mailServer)
+	log.Debug("mail from: ", mailFrom)
+	log.Debug("mail to: ", mailTo)
 
-	twitterUser := config.Get("twitter.username").(string)
-	twitterPass := config.Get("twitter.password").(string)
+// 	twitterUser := config.Get("twitter.username").(string)
+// 	twitterPass := config.Get("twitter.password").(string)
 
-	dbPath := path.Join(dirname, "logs", "tweet.ldb")
-	logger.Debug("db path: ", dbPath)
+	dbPath := path.Join(homedir, "logs", "tweet.ldb")
+	log.Debug("db path: ", dbPath)
 
 	db, err := leveldb.OpenFile(dbPath, nil)
 	if err != nil {
@@ -117,37 +165,43 @@ func main() {
 	for iter.Next() {
 		key := iter.Key()
 		value := iter.Value()
-		logger.Debug("key: ", string(key))
-		logger.Debug("value: ", string(value))
+		log.Debug("key: ", string(key))
+		log.Debug("value: ", string(value))
 	}
 	iter.Release()
+	os.Exit(0)
+
+	jsonCookiesPath := path.Join(homedir, ".twcookies.json")
+	netscapeCookiesPath := path.Join(homedir, "Downloads", "x.com_cookies.txt")
+
+	var cookies []*http.Cookie
+	if _, err := os.Stat(jsonCookiesPath); err == nil {
+		cookies = ReadJSONCookies(jsonCookiesPath)
+	} else {
+		cookies = ReadNetscapeCookies(netscapeCookiesPath)
+	}
+	PrintCookies(cookies)
 
 	scraper := twitterscraper.New()
-
-	cookiesPath := path.Join(dirname, ".twcookies.json")
-	f, _ := os.Open(cookiesPath)
-	// deserialize from JSON
-	var cookies []*http.Cookie
-	json.NewDecoder(f).Decode(&cookies)
-	// load cookies
 	scraper.SetCookies(cookies)
-	if err := f.Close(); err != nil {
-		panic(err)
-	}
 
 	if !scraper.IsLoggedIn() {
-		err := scraper.Login(twitterUser, twitterPass)
-		if err != nil {
-			panic(err)
-		}
+// 		log.Debug("Logging in as user ", twitterUser)
+// 		err := scraper.Login(twitterUser, twitterPass)
+// 		if err != nil {
+// 			panic(err)
+// 		}
+		panic("Not Logger In")
 	}
+
+	log.Debug("Getting tweets.")
 
 	for tweet := range scraper.GetTweets(context.Background(), twitterHandle, 10) {
 		if tweet.Error != nil {
 			panic(tweet.Error)
 		}
 
-		logger.Debugf("tweet: %+v\n", tweet)
+		log.Debugf("tweet: %+v\n", tweet)
 
 		msgText := fmt.Sprintf(
 			"%s <%s> %s\n%s",
@@ -157,7 +211,7 @@ func main() {
 			tweet.PermanentURL,
 		)
 		msgText = html.UnescapeString(msgText)
-		logger.Debug("msg text: ", msgText)
+		log.Debug("msg text: ", msgText)
 
 		tweetExists, _ := db.Has([]byte(tweet.ID), nil)
 		if !tweetExists {
@@ -170,6 +224,6 @@ func main() {
 	// serialize to JSON
 	js, _ := json.Marshal(new_cookies)
 	// save to file
-	f, _ = os.Create(cookiesPath)
+	f, _ := os.Create(jsonCookiesPath)
 	f.Write(js)
 }
